f4ea267ac184b86a13b361371593f67e4849059b
Abdul Rehman <abrehman4163@gmail.com>
2024-06-17 03:32:12 +0500

    NavBar added

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './NavBar.css'
+// import Logo from '../../Assets/Logo.png'
+function NavBar() {
+  return (
+    <div className='navBar'>
+
+      {/* <img src={Logo} alt='Abdul Rehman'/> */}
+
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className="nav-connect">
+        Connect with me
+      </div>
+      
+    </div>
+  )
+}
+
+export default NavBar
